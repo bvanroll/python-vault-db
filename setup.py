f960d43543f3a9202b3de0188a5548c4996af6ef
bvanroll <beppe.vanrolleghem@gmail.com>
2021-03-24 11:16:02 +0100

    depth is important maybe?

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "VaultDb"},
+    packages=setuptools.find_packages(where="VaultDb"),
     python_requires=">=3.6",
 )
